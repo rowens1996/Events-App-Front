4839b4e5ac210496f51abfd47f4b1755773825b9
Ryan <96872747+rowens1996@users.noreply.github.com>
2022-02-25 13:37:43 +0000

    Updates

---
+++ precache-manifest.06cf64869c8c0326b65755d6d84a7060.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc7f1fd19eeb89094dbacd576c153ac8",
+    "url": "/Events-App-Front/index.html"
+  },
+  {
+    "revision": "2178012fc1e806d7528b",
+    "url": "/Events-App-Front/static/css/2.d60ce58c.chunk.css"
+  },
+  {
+    "revision": "3009b6138f44bd96d002",
+    "url": "/Events-App-Front/static/css/main.514ad7b5.chunk.css"
+  },
+  {
+    "revision": "2178012fc1e806d7528b",
+    "url": "/Events-App-Front/static/js/2.c5a3325e.chunk.js"
+  },
+  {
+    "revision": "ae6bb62977b25ec73f0509e3cbccba19",
+    "url": "/Events-App-Front/static/js/2.c5a3325e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3009b6138f44bd96d002",
+    "url": "/Events-App-Front/static/js/main.a71abbc3.chunk.js"
+  },
+  {
+    "revision": "0107905e9a1e72b9cf3e",
+    "url": "/Events-App-Front/static/js/runtime-main.1d58b9ce.js"
+  }
+]);
